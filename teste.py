a325934f2e44af7929f9df9399459e3cb0ea5794
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-06 17:20:22 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,18 +5,6 @@ from json import loads
 
 
 
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #D3D3D3;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-
 
 #Criando a Pagina Inicial
 st.markdown('''
@@ -27,6 +15,17 @@ st.markdown('''
 Essa aba é direcionada a armazenar as informações do seu circuito elétrico, na qual estas, serão usadas para encontrar o disjuntor e bitola ideal para o seu sistema.
 Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidades.
 Conduto, o material e contas disponibilizados ainda assim devem ser revisados por um profisional cetificado.  
+
+<br>
+<br>
+<br>
+
+#### **Trabalho Idealizado e Desenvolvido por: **  <br>
+- Emelyn  
+- José David   
+- Daniel Bravin  
+- Gustavo Matos   
+
 ''')	
 
 
