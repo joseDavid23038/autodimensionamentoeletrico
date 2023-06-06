fa9bab548dfb7029587457d021ba187801dbc8ec
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-06 15:02:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,6 @@
 import streamlit as st
 st.sidebar.title("MENU")
-st.sidebar("Método")
+st.sidebar.subtitle("Método")
 st.title("Autodimensionamento")
 st.selectbox("Qual o Metodo Utilizado",["Metodo A","Método B","Método C","Metodo D"])
-
 st.button("B")
