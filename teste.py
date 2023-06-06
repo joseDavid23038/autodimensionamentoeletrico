55b7a11cdbb52d44370a1ca982ffe9b1572ea023
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-06 16:22:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,12 +5,12 @@ from json import loads
 
 #Criando a Pagina Inicial
 st.markdown('''
-# **Bem vindo**
+# **!! Bem vindo !!**
 ### Redimensionamneto de Circuito Elétricos
 
-dssadasd
-dsadas
-dsada
+Essa aba é direcionada a armazenar as informações do seu circuito elétrico, na qual estas, serão usadas para encontrar o disjuntor e bitola ideal para o seu sistema.
+Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidade.
+Conduto, o material e contas ainda assim devem ser revisados por um profisional cetificado.  
 ''')	
 
 # Criando uma Barra Lateral 
