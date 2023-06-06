ae5c76a0df9e1d6993be09ee03fee3cf4f5b7e0a
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-06 16:17:48 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,15 +6,12 @@ from json import loads
 #Criando a Pagina Inicial
 st.markdown('''
 # **Bem vindo**
-### Redimensionamneto de Circuito Elétricos 
-''')
-arquivo = st.file_uploader("arquivo aqui")
-if arquivo:
-  st.image(arquivo)	
-  
-else:
-  st.error("Arraste um arquivo")
-		
+### Redimensionamneto de Circuito Elétricos
+
+dssadasd
+dsadas
+dsada
+''')	
 
 # Criando uma Barra Lateral 
 st.sidebar.title("MENU")
