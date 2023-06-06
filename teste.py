48bd1e80b1ab3615c8376661e3b32cb2e3b27277
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-06 15:00:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,5 +3,5 @@ st.sidebar.title("MENU")
 st.sidebar("Método", "Tensão", "Itens na Rede")
 st.title("Autodimensionamento")
 st.selectbox("Qual o Metodo Utilizado",["Metodo A","Método B","Método C","Metodo D"])
-bol B = FALSE
+
 st.button("B")
