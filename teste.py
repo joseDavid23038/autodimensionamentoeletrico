f2780ff320c14fd2f1cf43a61017c19c9edac007
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-06 16:25:18 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,15 +6,15 @@ from json import loads
 #Criando a Pagina Inicial
 st.markdown('''
 # **!! Bem vindo !!**
-### Redimensionamneto de Circuito Elétricos
+### Redimensionamento de Circuito Elétricos
 
 Essa aba é direcionada a armazenar as informações do seu circuito elétrico, na qual estas, serão usadas para encontrar o disjuntor e bitola ideal para o seu sistema.
-Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidade.
-Conduto, o material e contas ainda assim devem ser revisados por um profisional cetificado.  
+Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidades.
+Conduto, o material e contas disponibilizados ainda assim devem ser revisados por um profisional cetificado.  
 ''')	
 
 # Criando uma Barra Lateral 
 st.sidebar.title("MENU")
 
-st.selectbox("Qual o Metodo Utilizado",["Metodo A","Método B","Método C","Metodo D"])
+st.sidebar.select_text("Qual o Metodo Utilizado",["Metodo A","Método B","Método C","Metodo D"])
 st.button("B")
