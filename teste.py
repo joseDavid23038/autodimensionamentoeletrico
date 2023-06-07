a45852869f45789e46adddc190006c4952356eb9
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-07 11:24:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -63,8 +63,8 @@ if stage_page == 2:
       tensao = st.selectbox("Tensão", [127, 220,380])
       potencia = st.slider("Potência Total do Circuito", min_value=0, max_value=26000)
       corrente = potencia/tensao
-      print(tensao*2)
-      print(potencia*2)
+      st.write(tensao*2)
+      st.write(potencia*2)
       print(corrente)
      
       num_circuitos = st.slider("Circuitos no mesmo eletrodulto", min_value=1, max_value=30)
