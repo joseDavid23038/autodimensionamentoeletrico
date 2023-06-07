e19118bfd22da526d264a734e65eb5d26daf583b
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-06 22:33:13 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -12,17 +12,24 @@ redi = st.container()
 
 
 # Criando a Pagina Inicial____________________________________________________________________________________________________________________________________________________________
-with inicial:
-  st.markdown('''
 
-  # Bem vindo
-  ### Redimensionamento de Circuito Elétricos
+if pagina_inicial:
+  inicial.empty()
+  with inicial:
+    st.markdown('''
 
-  Essa aba é direcionada a armazenar as informações do seu circuito elétrico, na qual estas, serão usadas para encontrar o disjuntor e bitola ideal para o seu sistema.
-  Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidades.
-  Conduto, o material e contas disponibilizados ainda assim devem ser revisados por um profisional cetificado.  
+    # Bem vindo
+    ### Redimensionamento de Circuito Elétricos
 
-  ''')
+    Essa aba é direcionada a armazenar as informações do seu circuito elétrico, na qual estas, serão usadas para encontrar o disjuntor e bitola ideal para o seu sistema.
+    Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidades.
+    Conduto, o material e contas disponibilizados ainda assim devem ser revisados por um profisional cetificado.  
+
+    ''')
+if pagina_redi:
+  redi.empty()
+  with redi
+    st.button("Teste")
 
 dica = False
 
@@ -42,8 +49,11 @@ bar = st.sidebar
 
 bar.title("MENU")
 
-barra_opcções = bar.selectbox(
-  "Ir para",
-  ["Pagina Inicial","Redimensionamento","Sobre o Projeto"])
+pagina_inicial = bar.button("Página Inicial")
+pagina_redi = bar.button("Redimensionamento")
+pagina_sobre = bar.buttin("Sobre o Projeto")
+
+  
+
 
 #____________________________________________________________________________________________________________________________________________________________________________
