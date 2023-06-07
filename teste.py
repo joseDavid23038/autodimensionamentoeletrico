685911ca4160e5b05bf9e7e30b4385bb62543ee7
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-06 22:26:21 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -17,14 +17,14 @@ redi = st.container()
 with inicial:
   st.markdown('''
 
-# Bem vindo
-### Redimensionamento de Circuito Elétricos
+  # Bem vindo
+  ### Redimensionamento de Circuito Elétricos
 
-Essa aba é direcionada a armazenar as informações do seu circuito elétrico, na qual estas, serão usadas para encontrar o disjuntor e bitola ideal para o seu sistema.
-Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidades.
-Conduto, o material e contas disponibilizados ainda assim devem ser revisados por um profisional cetificado.  
+  Essa aba é direcionada a armazenar as informações do seu circuito elétrico, na qual estas, serão usadas para encontrar o disjuntor e bitola ideal para o seu sistema.
+  Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidades.
+  Conduto, o material e contas disponibilizados ainda assim devem ser revisados por um profisional cetificado.  
 
-''')
+  ''')
 
 dica = False
 
