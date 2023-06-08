257b11726158cbb8968ee1aa5c729c59af5bf9e0
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-08 11:09:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -43,6 +43,15 @@ entrada, saida = st.tabs(["Entrada","Saída"])
 
 with entrada:
   st.markdown('''
+
+    # Bem vindo
+
+    Essa aba é direcionada a armazenar as informações do seu circuito elétrico, na qual estas, serão usadas para encontrar o disjuntor e bitola ideal para o seu sistema.
+    Lembresse, aarmação e montagem de todo aparato elétrico deve ser realizada por um profissional, nosso trabalho é encontrar uma escala de produto que satisfaça suas necessidades.
+    Conduto, o material e contas disponibilizados ainda assim devem ser revisados por um profisional cetificado.  
+
+    ''')
+  st.markdown('''
   ### Informe suas Variáveis:
   ###### Aba direcionada a receber as variaveis de seus circúitos.
   ''')
