9f9418540b504fb33103d6cf36ffcc510e8c09d4
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-08 11:03:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -69,21 +69,21 @@ with entrada:
       st.write("A Tensão é referente a tensão de linha do seu circuito, consulte o seu provedor de energia caso desconheça")
       
     st.markdown(''' 
-    ###### Sobre a Pôtencia:
+    ###### Sobre a Potência:
     ''')
-    if st.button("?", type="primary", key="Método"):
+    if st.button("?", type="primary", key="Potência"):
       st.write("Informe a potência total do circuito, caso desconheca clique em não sei e siga as instruções")
       
     st.markdown(''' 
     ###### Sobre o Circuitos:
     ''')
-    if st.button("?", type="primary", key="Método"):
+    if st.button("?", type="primary", key="Circuitos"):
       st.write("Informe o numero circuitos ou condutores dentro do eletroduto")
       
     st.markdown(''' 
     ###### Sobre a Temperatura:
     ''')
-    if st.button("?", type="primary", key="Método"):
+    if st.button("?", type="primary", key="Temperatura"):
       st.write("Informe a temperatura média do local da instalação, ou mesmo da sua região")
   
 #______________________________________________________________________________________________________________________________________________________________________________
