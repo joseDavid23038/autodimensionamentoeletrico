9ec8ed5ff535bd53c8effd5bbb3267c5b2a64a23
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-13 12:48:31 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -57,6 +57,7 @@ with entrada:
   ''')
   variaveis, ajuda = st.columns([4,1])
   with variaveis:
+    condição = ""
     metodo_usado = st.selectbox("Método", ["A1","A2", "B1","B2", "C", "D"])
     tensao = st.selectbox("Tensão", [127, 220,380])
     potencia = st.number_input("Potência Total do Circuito", min_value=100, max_value=2000, value=1000)
