a9691119ff039634ce91e46a7dcdd162d1198660
MrBravin <135053727+MrBravin@users.noreply.github.com>
2023-06-20 16:17:52 -0300

    Debugging

--- pagina.py
+++ pagina.py
@@ -1,13 +1,8 @@
 #Importando Bibliotecas____________________________________________________________________________________________________________________________________________________________
 import streamlit as st
 #Importando funcoes.py
-
-import pandas as pd
-import numpy as np
-
-tabela_agrupamento = pd.read_excel("Dados\AGRUPAMENTO.xlsx")
-tabela_disjuntores = pd.read_excel("Dados\DISJUNTORES.xlsx")
-tabela_temperatura = pd.read_excel("Dados\TEMPERATURA.xlsx")
+import os
+print(os.listdir())
 
 from funcoes import *
 
