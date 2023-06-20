eb47a459ff4862820b42ab5aa6c531b734086e07
joseDavid23038 <135053261+joseDavid23038@users.noreply.github.com>
2023-06-20 15:39:45 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,14 +1,11 @@
-import pandas as pd #importa a biblioteca pandas (utilizada para analisar e organizar os dados)
-import numpy as np #importa a biblioteca numpy (utilizada para realizar os cálculos necessários para o dimensionamento elétrico)
+import pandas as pd
+import numpy as np
 
-import pip #importa o pip, um gerenciador das bibliotecas que iremos utilizar
-pip.main(["install", "openpyxl"]) #instala a biblioteca openpyxl, que consegue acessar, ler e criar arquivos .xlsx (Excel)
+tabela_disjuntores = pd.read_excel("Dados\DISJUNTORES.xlsx")
+tabela_agrupamento = pd.read_excel("Dados\AGRUPAMENTO.xlsx")
+tabela_temperatura = pd.read_excel("Dados\TEMPERATURA.xlsx")
 
-tabela_disjuntores = pd.read_excel("Dados/DISJUNTORES.xlsx") #acessa, por meio da biblioteca pandas, a tabela com os dados sobre disjuntores
-tabela_agrupamento = pd.read_excel("Dados/AGRUPAMENTO.xlsx") #acessa, por meio da biblioteca pandas, a tabela com os dados sobre o agrupamento de fios
-tabela_temperatura = pd.read_excel("Dados/TEMPERATURA.xlsx") #acessa, por meio da biblioteca pandas, a tabela com os dados sobre a temperatura do ambiente
-
-def condicao_de_instalacao(isolamento,local): 
+def condicao_de_instalacao(isolamento,local):
     if isolamento == "PVC" and (local == "Teto" or local =="Parede"):
        condicao = 'PVCAMBIENTE'
     if isolamento == "PVC" and local == "Solo":
@@ -24,60 +21,66 @@ def tabela_a_ser_usada(isolamento):
         tabela_usada = "DOIS_COBRE_PVC.xlsx" 
     if isolamento == "XLPE" or isolamento == "EPR":
         tabela_usada = "DOIS_COBRE_EPR_XLPE.xlsx" 
-    return pd.read_excel(tabela_usada)
+    return tabela_usada
 
 
-def disjuntor_inicial(P, V, disjuntores = tabela_disjuntores):
+def disjuntor_inicial(P, V, disjuntores = tabela_disjuntores ):
     """
-    Calcula o valor da intensidade de corrente. 
-    Args:
+    args:
         P: Potência 
         V: Tensão
-        Disjuntores: A tabela de disjuntores a ser usada na análise (opcional)
-    Returns: 
+        disjuntores: A tabela de disjuntores a ser usada na análise (opcional)
+    returns: 
         O disjuntor adequado para as condições especificadas.
     """
-    I = P/V #Calcula a intensidade de corrente (I), a partir dos argumentos P e V dados pelo usuário
-    for linha in disjuntores.itertuples(): #Itera a tabela de disjuntores pelo método .itertuples()
-        coluna = "DISJUNTOR" 
-        if getattr(linha, coluna) > I: #O método getattr analisa os valores da tabela. Se o valor obtido através da tabela for maior que o I
-            return getattr(linha, coluna) #Retorna o disjuntor adequado
+    I = P/V
+    for linha in disjuntores.itertuples():
+        coluna = "DISJUNTOR"
+        if getattr(linha, coluna) > I:
+            return getattr(linha, coluna)
 
 
 
 # def dimensionar(tabela, metodo = 'B1', Bitola_min = 2.5, FT = 0.5, P = 1550, V = 220):
-def fator_temperatura(condicao, temperatura_ambiente, temperaturas = tabela_temperatura):
+def fator_temperatura(condicao, temperatura_ambiente):
     """
-    Args:
+    args:
         condicao: Condição de instalação
         temperatura_ambiente: Temperatura média do amibiente. 
         temperaturas: A tabela de temperatura a ser usada na análise.
-    Returns: 
+    returns: 
         Fator de temperatura.
     """
-    for temperatura in temperaturas.itertuples():
-        temperatura_usada = getattr(temperatura, condicao)
-        if temperatura_usada > temperatura_ambiente:
-            return temperatura_usada
+    coluna_temperatura = 'TEMPERATURA'
+    linha_fator_temperatura = 0
+    temperatura = tabela_temperatura
+
+    maior_temperatura_mais_proxima = temperatura.loc[linha_fator_temperatura][coluna_temperatura]
+
+    while float(maior_temperatura_mais_proxima)<= temperatura_ambiente:
+        linha_fator_temperatura = linha_fator_temperatura+1
+        maior_temperatura_mais_proxima = temperatura.loc[linha_fator_temperatura][coluna_temperatura]
+        fator_temperatura = temperatura.loc[linha_fator_temperatura][condicao]
+    return float(fator_temperatura)
 
 
 def fator_agrupamento(n_circuitos, metodo, agrupamentos = tabela_agrupamento):
     """
-    Args:
+    args:
         n_circuitos: Número de circuitos a serem considerados.
         Método: Método de instalação usado.
         agrupamentos: A tabela de agrupamento a ser usada na análise.
-    Returns: 
+    returns: 
         Fator de temperatura.
     """
     return agrupamentos.loc[n_circuitos+1][metodo]
 
 def fator_correcao(agrupamento, temperatura):
     """
-    Args:
+    args:
         agrupamento: O fator de agrupamento.
         temperatura: O fator de temperatura
-    Returns: 
+    returns: 
         Fator de agrupamento.
     """
     return agrupamento*temperatura
@@ -85,10 +88,10 @@ def fator_correcao(agrupamento, temperatura):
 def bitola_min(tipo_instalacao):
     if tipo_instalacao == "Iluminação":
         bitola_min = 1.5
-    if tipo_instalacao == "Tomada de Uso Específico" or tipo_instalacao =="Tomadas de Uso Geral":
+    if tipo_instalacao == "Tomadas de Uso Específico" or tipo_instalacao =="Tomadas de Uso Geral":
         bitola_min = 2.5
     return bitola_min
-
+    
 def bitola(disjuntor, bitola_min, metodo, correcao, isolamento):
     """
     args:
@@ -100,7 +103,11 @@ def bitola(disjuntor, bitola_min, metodo, correcao, isolamento):
     returns: 
         finais: lista com disjuntor final e tamanho ideal de bitola.
     """
-    tabela = tabela_a_ser_usada(isolamento)
+    tabela_usada = tabela_a_ser_usada(isolamento)
+    if tabela_usada == "DOIS_COBRE_PVC.xlsx":
+        tabela = pd.read_excel("Dados\DISJUNTORES.xlsx")
+    if tabela_usada == "DOIS_COBRE_EPR_XLPE.xlsx":
+        tabela = pd.read_excel("Dados\DOIS_COBRE_EPR_XLPE.xlsx")
     bit = 'SEÇÃO'
     I_max = correcao * tabela[metodo].values
     fios = tabela[bit].values
@@ -117,25 +124,25 @@ def bitola(disjuntor, bitola_min, metodo, correcao, isolamento):
 
 
 if __name__ == "__main__":
-    
-    '''metodo = "A1"
+   
+    metodo = "A1"
     tipo_instalacao = "Iluminação"
     tensao = 127
-    potencia = 500
+    potencia = 2000
     num_circuitos = 4
     isolamento = "PVC"
     local = "Parede"
-    temperatura = 60 '''
+    temperatura_ambiente = 55 
 
    
     condicao = condicao_de_instalacao(isolamento,local)
     disjuntores = tabela_disjuntores
     disjuntor = disjuntor_inicial(potencia, tensao, disjuntores)
-    temperaturas = tabela_temperatura
-    ftemperatura = fator_temperatura(condicao, temperatura, temperaturas)
-    agrupamentos = tabela_agrupamento
-    agrupamento = fator_agrupamento(num_circuitos, metodo, agrupamentos)
+    ftemperatura = fator_temperatura(condicao, temperatura_ambiente)
+    agrupamento = fator_agrupamento(num_circuitos, metodo, agrupamentos = tabela_agrupamento)
     correcao = fator_correcao(agrupamento, ftemperatura)
     bitola_mn = bitola_min(tipo_instalacao)
     secao = bitola(disjuntor, bitola_mn, metodo, correcao, isolamento)
     print(secao)
+
+    
