1a211d030e79ba902a46836de387f24d5e7ab641
GustaMatos0 <programming.gustamatos@gmail.com>
2023-06-20 17:11:20 -0300

    Correção função bitola

--- funcoes.py
+++ funcoes.py
@@ -112,13 +112,16 @@ def bitola(disjuntor, bitola_min, metodo, correcao, isolamento):
     I_max = correcao * tabela[metodo].values
     fios = tabela[bit].values
     finais = []
-    for valor in I_max:
+    for valor, secao in zip(I_max, fios):
         if valor > disjuntor:
             finais.append(valor)
-            break
-    for valor in fios:
-        if valor >= bitola_min:
-            finais.append(valor)
+            if secao >= bitola_min:
+                finais.append(secao)
+            else:
+                for valor in fios:
+                    if valor >= bitola_min:
+                        finais.append(valor)
+                        break
             break
     return finais
 
@@ -141,8 +144,9 @@ if __name__ == "__main__":
     ftemperatura = fator_temperatura(condicao, temperatura_ambiente)
     agrupamento = fator_agrupamento(num_circuitos, metodo, agrupamentos = tabela_agrupamento)
     correcao = fator_correcao(agrupamento, ftemperatura)
+    print(correcao)
     bitola_mn = bitola_min(tipo_instalacao)
     secao = bitola(disjuntor, bitola_mn, metodo, correcao, isolamento)
-    print(secao[0])
+    print(secao[1])
 
     
